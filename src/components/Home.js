07970d957007fbb835011893d133991c0b872968
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-11 23:46:52 +0530

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home =()=>{
+    return (
+        <div>
+            <h1>H1</h1>
+            <p>P1</p>
+            <p>p2</p>
+        </div>
+    );
+}
+
+export default Home;
