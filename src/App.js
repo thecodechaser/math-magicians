0e8dc6b87d0be23d1516212028a189912a5da369
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-12 00:14:16 +0530

    Import all component to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 class App extends React.Component {
   render() {
