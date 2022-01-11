3c3322768cb4c8ff687035502398a05d5da9fd8e
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-11 23:56:50 +0530

    Complete Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,37 @@
 import React from "react";
+export { NavLink } from 'react-router-dom';
 
 const Navbar =()=> {
+    const links = [
+        {
+            id: 1,
+            path: "/home",
+            text: "Home"
+        },
+        {
+            id: 2,
+            path: "/calculator",
+            text: "Calculator"
+        },
+        {
+            id: 3,
+            path: "/quote",
+            text: "Quote"
+        }
+    ]
+
     return (
+        <nav>
         <ul>
-            <li>Li</li>
+           {
+               links.map((link) => {
+                   <li key={link.id}>
+                       <NavLink to={link.path}>{link.text}</NavLink>
+                   </li>
+               } )
+           }
         </ul>
+        </nav>
     );
 }
 
