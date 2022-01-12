a4b965ee8d2ad99c63539a5772f1457285fb50ea
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-12 15:34:18 +0530

    Add markup for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const Home =()=>{
     return (
-        <div>
-            <h1>H1</h1>
-            <p>P1</p>
-            <p>p2</p>
-        </div>
+        <main>
+            <h1>Welcome to our page!</h1>
+            <p>End of the day, life itself is a math problem. Everything can be explained through math, 
+                including the rotation of our Earth or creation of 
+                the human kind. You will also find some inspirational math quotes for students with donuts</p>
+            <p>There are certain calculations I should like to make with you,
+To be sure that your deductions will be logical and true;
+And remember, 'Patience, Patience,' is the watchword of a sage,
+Not to-day nor yet to-morrow can complete a perfect age.</p>
+        </main>
     );
 }
 
