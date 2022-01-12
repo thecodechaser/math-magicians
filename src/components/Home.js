49aaa01bf1ffca139b8fb5d235167f3adbc86dee
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-12 17:44:35 +0530

    Add styling for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Home =()=>{
     return (
-        <main>
-            <h1>Welcome to our page!</h1>
-            <p>End of the day, life itself is a math problem. Everything can be explained through math, 
+        <main className="home-main">
+            <h1 className="home-heading">Welcome to our page!</h1>
+            <p className="home-para">End of the day, life itself is a math problem. Everything can be explained through math, 
                 including the rotation of our Earth or creation of 
                 the human kind. You will also find some inspirational math quotes for students with donuts</p>
-            <p>There are certain calculations I should like to make with you,
+            <p className="home-para">There are certain calculations I should like to make with you,
 To be sure that your deductions will be logical and true;
 And remember, 'Patience, Patience,' is the watchword of a sage,
 Not to-day nor yet to-morrow can complete a perfect age.</p>
