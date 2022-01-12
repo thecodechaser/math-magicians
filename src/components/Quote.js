f9d7a773beafe5de57ee761bbdd16a4ff772f5a1
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-12 17:41:15 +0530

    Add styling for quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Quote =()=> {
     return (
-        <main>
-            <p>Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers.
+        <main class="quote-main">
+            <p class="quote">Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers.
 — Shakuntala Devi, Indian writer and mental calculator</p>
         </main>
     );
